4147b2c61cc61c822213494b71c087ddf598a3d2
EC2 Default User <ec2-user@ip-172-31-33-62.eu-west-1.compute.internal>
2018-11-30 15:54:10 +0000

    Test

--- db/migrate/20181127155442_create_subjects.rb
+++ db/migrate/20181127155442_create_subjects.rb
@@ -2,8 +2,8 @@ class CreateSubjects < ActiveRecord::Migration[5.2]
   def change
     create_table :subjects do |t|
       t.string :classname
-      t.references :course, foreign_key: true
-      t.references :faculty, foreign_key: true
+      ##t.references :course, foreign_key: true
+      ##t.references :faculty, foreign_key: true
 
       t.timestamps
     end
